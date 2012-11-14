2fad4067dd13ebd150e30a51f6d01b9c96c01b47
alaingilbert <alain.gilbert.15@gmail.com>
2012-11-13 20:30:53 -0500

    pymongo test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
+import os
 import time
 import pymongo
 
@@ -11,14 +12,19 @@ def do_work():
 
 
 def main():
-    while True:
-        try:
-            do_work()
-            time.sleep(10 * 60)
-        except KeyboardInterrupt, err:
-            raise err
-        except Exception, err:
-            print 'ERROR: ' + str(err)
+    DATA_DB = os.environ['DATA_DB']
+    connection = pymongo.Connection(DATA_DB)
+    db = connection.seaoo
+    notices = db.notices
+    print notices.count()
+    #while True:
+    #    try:
+    #        do_work()
+    #        time.sleep(10 * 60)
+    #    except KeyboardInterrupt, err:
+    #        raise err
+    #    except Exception, err:
+    #        print 'ERROR: ' + str(err)
 
 
 if __name__ == '__main__':
